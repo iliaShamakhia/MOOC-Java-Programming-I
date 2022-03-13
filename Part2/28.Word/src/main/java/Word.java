7e0f805f33412e1289ebe67a67890becafcc8328
iliaShamakhia <shamakhiailia@gmail.com>
2022-03-13 13:59:07 +0400

    exercises 26-30

---
+++ Part2/28.Word/src/main/java/Word.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String hello = word();
+        System.out.println(hello);
+    }
+    
+    public static String word(){
+        return "Hello World!";
+    }
+}
